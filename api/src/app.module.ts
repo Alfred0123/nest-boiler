fa7284a2b279b9de5837e9c4136f7ef5925c97b3
hyobumjung <hyobumjung@houstep.co.kr>
2022-09-18 20:38:50 +0900

    Feat: path alias 적용

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import configuration from './config/config.service';
+import configuration from '@/config/config.service';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true, load: [configuration] })],
